e5bc85bebbe4c5c2e3c92da7b23c632ea7bc61a9
Katie Foster <fosterkatiea@gmail.com>
2020-09-22 13:07:06 -0500

    fix test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from './main';
 
-test('renders learn react link', () => {
+test('renders real Factor link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Go to the Real Factor_/i);
   expect(linkElement).toBeInTheDocument();
 });
